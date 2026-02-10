1a78b159ee0ba770883ffbaf66af325601664688
Tinku Kumar <tku739@gmail.com>
2026-02-11 05:23:03 +0530

    fix the local call

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 // src/services/api.ts
-const API_URL = "http://localhost:9050/v1/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:9050/v1/api";
 
 const getHeaders = () => {
   const token = localStorage.getItem("adminToken");
